0a30d5c83231157298f067555c2a8b95b8d65613
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-13 01:55:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
  
   get '/name' do 
-    return 200
-  return "My name is #{name}"
+   [ return 200, "My name is #{name}"]
+ 
   end
   
 end
